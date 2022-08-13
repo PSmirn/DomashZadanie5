d512788ac975df8a8f41c3d5f91bab8d9a7ff9cd
Pasha <pashakasm@mail.ru>
2022-08-13 09:16:00 +0300

    reshenie

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿
+void Zadacha41()
+{
+    Console.Write("Количество цифр, которых хотите ввести: " );
+    int Count = int.Parse(Console.ReadLine());
+    int[] array = new int[Count];
+    int KolMaxZero = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write("Введите число: ");
+        array[i] =  int.Parse(Console.ReadLine());
+        if (array[i] > 0)
+        {
+          KolMaxZero += 1;  
+        }
+    }
+    Console.WriteLine("Чисел больше нуля: " + KolMaxZero);    
+}
+void Zadacha43()
+{
+    Console.Write("Введите значение b1 = ");
+    double b1 = int.Parse(Console.ReadLine());
+    Console.Write("Введите значение k1 = ");
+    double k1 = int.Parse(Console.ReadLine());    
+    Console.Write("Введите значение b2 = ");
+    double b2 = int.Parse(Console.ReadLine());
+    Console.Write("Введите значение k2 = ");
+    double k2 = int.Parse(Console.ReadLine());
+
+    if (k1 == k2)
+    {
+       Console.WriteLine("Прямые паралельны.");
+    }
+    else
+    {
+    double x = (b1 - b2)/(k1 - k2);
+    double y = (k1 * x) + b1;
+    Console.Write("Координаты точки: " + x +", "+ y);
+    }
+}
+
+Zadacha43();
